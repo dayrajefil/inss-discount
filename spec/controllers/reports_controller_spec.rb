0aeb5e34dc434acc88eff5d00ee7216da92f871f
dayrajefil <dayrajefil@gmail.com>
2024-11-29 16:38:56 -0300

    fix reports controller test

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -11,14 +11,15 @@ RSpec.describe ReportsController, type: :controller do
       create(:proponent, salary: 6000.00)
     end
 
-    it 'agrupa corretamente os proponentes por faixa salarial' do
+    it 'correctly groups bidders by salary range' do
       get :proponent_salary
 
       expect(assigns(:salary_groups)).to eq([
                                               { label: 'Até R$ 1.045,00', count: 1 },
                                               { label: 'De R$ 1.045,01 a R$ 2.089,60', count: 1 },
                                               { label: 'De R$ 2.089,61 até R$ 3.134,40', count: 1 },
-                                              { label: 'De R$ 3.134,41 até R$ 6.101,06', count: 2 }
+                                              { label: 'De R$ 3.134,41 até R$ 6.101,06', count: 2 },
+                                              { label: 'Acima de R$ 6.101,06', count: 0 }
                                             ])
     end
   end
