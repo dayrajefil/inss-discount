d0756fff2107dcd9cc6c673ff9155307cbf8a232
dayrajefil <dayrajefil@gmail.com>
2025-04-04 20:36:11 -0300

    INSS calculation correction

--- app/services/inss_calculator.rb
+++ app/services/inss_calculator.rb
@@ -9,21 +9,27 @@
 class InssCalculator
   def self.calculate(salary)
     brackets = [
-      { limit: 0.00, rate: 0.075 },
-      { limit: 1_045.00, rate: 0.09 },
-      { limit: 2_089.60, rate: 0.12 },
-      { limit: 3_134.40, rate: 0.14 },
-      { limit: 6_101.06, rate: 0.14 }
+      { limit: 1_412.00, rate: 0.075 },
+      { limit: 2_666.68, rate: 0.09 },
+      { limit: 4_000.03, rate: 0.12 },
+      { limit: 7_786.02, rate: 0.14 },
+      { limit: Float::INFINITY, rate: 0.14 }
     ]
 
     total_inss = 0
     remaining_salary = salary
 
     brackets.each_with_index do |bracket, index|
-      next_limit = brackets[index + 1]&.[](:limit) || remaining_salary
+      current_limit = if brackets[index][:limit] == Float::INFINITY
+                        remaining_salary
+                      else
+                        brackets[index][:limit]
+                      end
 
-      if remaining_salary > bracket[:limit]
-        taxable_income = [next_limit, remaining_salary].min - bracket[:limit]
+      last_limit = index.zero? ? 0.0 : brackets[index - 1][:limit]
+
+      unless remaining_salary < last_limit
+        taxable_income = (current_limit - last_limit).round(2)
         total_inss += (taxable_income * bracket[:rate]).round(2)
       end
     end
