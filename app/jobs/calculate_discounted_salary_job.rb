feae5dfbbade2e16ceee21d9be86cb6e5e17c079
dayrajefil <dayrajefil@gmail.com>
2025-04-04 20:35:47 -0300

    Preventing errors in salary deduction calculator job

--- app/jobs/calculate_discounted_salary_job.rb
+++ app/jobs/calculate_discounted_salary_job.rb
@@ -13,8 +13,12 @@ class CalculateDiscountedSalaryJob < ApplicationJob
   queue_as :default
 
   def perform(proponent_id)
-    proponent = Proponent.find(proponent_id)
+    proponent = Proponent.find_by(id: proponent_id)
+    return false unless proponent
+
     discounted_salary = proponent.salary - proponent.inss
-    proponent.update(discounted_salary: discounted_salary)
+    success = proponent.update(discounted_salary: discounted_salary)
+
+    success || proponent.errors.full_messages
   end
 end
