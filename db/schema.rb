f4375dd4ca1121dcb678a46837cff89bf2e2a8a1
dayrajefil <dayrajefil@gmail.com>
2025-04-04 20:37:04 -0300

    Improving readability and changing to single quotation marks

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,47 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_241_130_142_350) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_30_142350) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'addresses', force: :cascade do |t|
-    t.string 'zip_code', null: false
-    t.string 'street', null: false
-    t.string 'number', null: false
-    t.string 'complement'
-    t.string 'neighborhood', null: false
-    t.string 'city', null: false
-    t.string 'state', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "addresses", force: :cascade do |t|
+    t.string "zip_code", null: false
+    t.string "street", null: false
+    t.string "number", null: false
+    t.string "complement"
+    t.string "neighborhood", null: false
+    t.string "city", null: false
+    t.string "state", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'proponents', force: :cascade do |t|
-    t.string 'name'
-    t.date 'birthdate'
-    t.string 'phone'
-    t.decimal 'salary', precision: 10, scale: 2
-    t.bigint 'address_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.decimal 'inss', precision: 10, scale: 2
-    t.decimal 'discounted_salary', precision: 10, scale: 2
-    t.index ['address_id'], name: 'index_proponents_on_address_id'
+  create_table "proponents", force: :cascade do |t|
+    t.string "name"
+    t.date "birthdate"
+    t.string "phone"
+    t.decimal "salary", precision: 10, scale: 2
+    t.bigint "address_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.decimal "inss", precision: 10, scale: 2
+    t.decimal "discounted_salary", precision: 10, scale: 2
+    t.index ["address_id"], name: "index_proponents_on_address_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'username', null: false
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[email username], name: 'index_users_on_email_and_username', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email", "username"], name: "index_users_on_email_and_username", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'proponents', 'addresses'
+  add_foreign_key "proponents", "addresses"
 end
